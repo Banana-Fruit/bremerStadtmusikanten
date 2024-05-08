c9827a6d73c4e73631d65176f4386b7710984551
micaelo <143270275+maremike@users.noreply.github.com>
2024-05-08 10:51:08 +0200

    Delete bremerStadtmusikanten directory

--- bremerStadtmusikanten/src/control/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.bremerstadtmusikanten {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.bremerstadtmusikanten to javafx.fxml;
-    exports org.example.bremerstadtmusikanten;
-}
