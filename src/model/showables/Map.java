00baee249d395da051bba33fefe148807a313826
Michael Markov <mk.markov@outlook.com>
2024-05-24 17:34:10 +0200

    Added code from Jonas to Map

--- src/model/showables/Map.java
+++ src/model/showables/Map.java
@@ -9,33 +9,70 @@ import model.Obstacle;
 import model.Player;
 import resources.Constants_DefaultValues;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 
 public class Map extends Showable
 {
-    private List<Obstacle> obstacles;
-    private List<Collectable> collectables;
-    private Image mapImage;
-    private Player player;
+    private char[][] tileChars;
+    private int maxScreenCol;
+    private int maxScreenRow;
     
-    
-    public Map(Scene scene, String backgroundPath)
+    public Map(int maxScreenCol, int maxScreenRow)
     {
-        super(scene, backgroundPath);
-        init();
+        this.maxScreenCol = maxScreenCol;
+        this.maxScreenRow = maxScreenRow;
+        tileChars = new char[maxScreenCol][maxScreenRow];
     }
     
-    
-    public void init()
+    public void loadMap(String path)
     {
-        this.player = new Player(
-                Constants_DefaultValues.DEFAULT_X_POSITION,
-                Constants_DefaultValues.DEFAULT_Y_POSITION,
-                Constants_DefaultValues.HITBOX_RADIUS,
-                new Image("/resources/player.jpg")
-        );
+        InputStream is = getClass().getResourceAsStream(path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
         
-        addImageView(new ImageView(player.getPlayerImage()), 10);
+        int row = 0;
+        try
+        {
+            String line;
+            while ((line = br.readLine()) != null && row < maxScreenRow)
+            {
+                char[] characters = line.toCharArray();
+                for (int col = 0; col < maxScreenCol && col < characters.length; col++)
+                {
+                    tileChars[col][row] = characters[col];
+                }
+                row++;
+            }
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        } finally
+        {
+            try
+            {
+                br.close();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public char[][] getTileChars()
+    {
+        return tileChars;
+    }
+    
+    public char getTileCharAt(int x, int y)
+    {
+        if (x < 0 || x >= tileChars.length || y < 0 || y >= tileChars[0].length)
+        {
+            return ' '; // Return an empty space if coordinates are out of bounds
+        }
+        return tileChars[x][y];
     }
 }
