12d4e79c5ed0d37903cce2feb19079044f247aa3
Michael Markov <mk.markov@outlook.com>
2024-05-26 17:17:07 +0200

    Added template exception for future exceptions.

---
+++ src/model/exceptions/CustomException.java
@@ -0,0 +1,14 @@
+package model.exceptions;
+
+
+/**
+ * This is a template exception for future exceptions. It is not used yet
+ */
+public class CustomException extends Exception
+{
+    
+    public CustomException (String message)
+    {
+        super(message);
+    }
+}
