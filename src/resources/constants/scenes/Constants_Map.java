ba9bd769740feaf6e66738b91d6b3a391797c467
Michael Markov <mk.markov@outlook.com>
2024-06-25 10:04:22 +0200

    Größe des Spielers angepasst.

--- src/resources/constants/scenes/Constants_Map.java
+++ src/resources/constants/scenes/Constants_Map.java
@@ -19,7 +19,7 @@ public interface Constants_Map
     int STARTPOSITION_Y = 25;
     float PLAYER_SPEED = 1;
     float SPEED_MULTIPLIER = 6f;
-    double PLAYER_SIZE = ValueConversion.getDiagonalSizeFromSquareLength(16);
+    double PLAYER_SIZE = ValueConversion.getDiagonalSizeFromSquareLength(TILE_SIZE);
     double ADJUST_DIAGONAL_MOVEMENT = Math.sqrt(2) / 2;
 
     // Map
